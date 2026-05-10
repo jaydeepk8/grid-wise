aea043c3835511af90ddd13d51a14282c19de223
Jaydeep Kulkarni <jay.kulkarni2005@gmail.com>
2026-05-10 23:13:59 +0530

    fix: replace hardcoded localhost:8000 with NEXT_PUBLIC_API_URL

--- gridwise/src/app/predict/page.js
+++ gridwise/src/app/predict/page.js
@@ -28,7 +28,7 @@ export default function PredictPage() {
     setResult(null);
 
     try {
-      const res = await fetch("http://localhost:8000/predict", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/predict`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
