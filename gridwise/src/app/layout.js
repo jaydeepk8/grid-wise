9a5174f54da20bacf9029614dd58fc8b20b1d36d
jaydeepk8 <jay.kulkarni2005@gmail.com>
2026-05-29 10:33:04 +0530

    fix: load Material Symbols via preconnect link in head with correct URL

--- gridwise/src/app/layout.js
+++ gridwise/src/app/layout.js
@@ -17,6 +17,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap"
+          rel="stylesheet"
+        />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#4a6741" />
